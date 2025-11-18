be765944ab9d10cb577cb2821d40db57e8d82f9c
muntajshaik <muntaj.shaik@thoughtworks.com>
2025-11-18 17:04:52 +0530

    CP-7: downgraded Spring Boot version to restore SpringDoc compatibility & fix OpenAPI 500 error

--- server/src/main/java/com/tw/coupang/one_payroll/paygroups/dto/request/PayGroupCreateRequest.java
+++ server/src/main/java/com/tw/coupang/one_payroll/paygroups/dto/request/PayGroupCreateRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Getter
 public class PayGroupCreateRequest {
 
     @NotBlank(message = "groupName must not be blank")
--- server/src/main/java/com/tw/coupang/one_payroll/paygroups/dto/response/PayGroupResponse.java
+++ server/src/main/java/com/tw/coupang/one_payroll/paygroups/dto/response/PayGroupResponse.java
@@ -1,10 +1,14 @@
 package com.tw.coupang.one_payroll.paygroups.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PayGroupResponse {
     private Integer payGroupId;
 }
