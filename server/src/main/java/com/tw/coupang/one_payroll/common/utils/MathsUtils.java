c04e0c461525ba842d7f0c353ce71f36d4fec07c
bhawesh.singh <bhawesh.singh@thoughtworks.com>
2025-12-02 17:26:08 +0530

    sonar fix

--- server/src/main/java/com/tw/coupang/one_payroll/common/utils/MathsUtils.java
+++ server/src/main/java/com/tw/coupang/one_payroll/common/utils/MathsUtils.java
@@ -6,6 +6,10 @@ import static java.math.RoundingMode.HALF_UP;
 
 public class MathsUtils {
 
+    private MathsUtils() {
+        // private constructor to prevent instantiation
+    }
+
     public static BigDecimal percentOf(final BigDecimal percent, final BigDecimal amount) {
         if (percent == null) return BigDecimal.ZERO;
         return amount.multiply(percent).divide(BigDecimal.valueOf(100), 2, HALF_UP);
