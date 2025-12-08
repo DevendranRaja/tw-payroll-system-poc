fe216a8ff662405670eb3eb1294d7045f8dd2aec
muntajshaik <muntaj.shaik@thoughtworks.com>
2025-12-08 18:01:48 +0530

    CP-17: removed unused import

--- server/src/test/java/com/tw/coupang/one_payroll/payroll/service/PayrollCalculationServiceImplTest.java
+++ server/src/test/java/com/tw/coupang/one_payroll/payroll/service/PayrollCalculationServiceImplTest.java
@@ -47,7 +47,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
