a10a078e72b63618aaadad47c3c5bb4f78bfb659
Devendran Raja <devendran.raja@thoughtworks.com>
2025-12-08 14:33:03 +0530

    [CP41]: considering decimal for hours

--- server/src/main/java/com/tw/coupang/one_payroll/timesheet/dto/TimesheetRequest.java
+++ server/src/main/java/com/tw/coupang/one_payroll/timesheet/dto/TimesheetRequest.java
@@ -1,6 +1,7 @@
 package com.tw.coupang.one_payroll.timesheet.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
