d9bc36f972c1ef8e37c2f3564467a53e5e9937a3
harshitapant <harshita.pant@thoughtworks.com>
2025-12-09 17:40:21 +0530

    CP-34: Adding Active profile to check for one of the failing Test

--- server/src/test/java/com/tw/coupang/one_payroll/userauth/config/SecurityPropertiesTest.java
+++ server/src/test/java/com/tw/coupang/one_payroll/userauth/config/SecurityPropertiesTest.java
@@ -3,10 +3,12 @@ package com.tw.coupang.one_payroll.userauth.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SecurityPropertiesTest {
 
     @Autowired
